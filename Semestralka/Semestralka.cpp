40f42ea7ae6633f08a71aafe7cc63128fc5d7682
Trolleye <petovorcak@gmail.com>
2025-04-16 13:34:52 +0200

    pre istotu commit

--- Semestralka/Semestralka.cpp
+++ Semestralka/Semestralka.cpp
@@ -1,10 +1,22 @@
 #include "RoutingTableHierarchy.h"
 int main() {
 	initHeapMonitor();
+	std::string userNextHop;
+	std::string userAddress;
+	int userMask;
+	std::string userFromTime;
+	std::string userToTime;
+
 	RoutingTableHierarchy hierarchy = RoutingTableHierarchy();
 	hierarchy.addFromVector();
 	auto iterator = RoutingTableHierarchyIterator(hierarchy.getHierarchy()->accessRoot(), hierarchy.getHierarchy());
-	
+	auto matchNextHopPred = [&](RoutingRecord& record) { return record.matchNextHop(userNextHop); };
+	auto matchWithAddressPred = [&](RoutingRecord& record) {return record.matchWithAddress(userAddress, userMask); };
+	auto matchLifetimePred = [&](RoutingRecord& record) {return record.matchLifeTime(userFromTime, userToTime); };
+	auto isLeaf = [&](RoutingRecordNode& record) { return record.getRecords().size() > 0;};
+
+
+
 	while (true) {
 		std::string input;
 		std::cin >> input;
